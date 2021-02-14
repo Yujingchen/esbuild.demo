8967f2a4913461b5efe4a8313abe7886795b2888
Yujing <vit7david@outlook.com>
2021-02-14 11:17:11 +0200

    buid text area and submit button

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react'
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return (<h1>hi</h1>)
-}
+    const [value, setValue] = useState('');
+    const [code, setCode] = useState('');
+    const onClick = () => {
+        console.log(value)
+        setCode('transplied code')
+    }
+    return (
+        <div>
+            <textarea value={value} onChange={(e) => setValue(e.target.value)}>
+            </textarea>
+            <div>
+                <button onClick={onClick}>
+                    Submit
+                 </button>
+            </div>
+            <pre>{code}</pre>
+        </div>)
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'))
