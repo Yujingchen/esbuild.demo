8a0cfdffec01d49f04c9d3f7e4c2a29bc907f335
Yujing <vit7david@outlook.com>
2021-02-20 16:04:12 +0200

    move bundler logic to a new file

--- src/component/previou.tsx
+++ src/component/previou.tsx
@@ -4,24 +4,27 @@ interface PreviewProps {
 }
 
 const html = `
-<html>
-<head></head>
-<body>
-<div id="root"></div> 
-</body>
-<script>
-window.addEventListener('message',(event)=>{
-    try{
-    eval(event.data)
-    }
-    catch(err){
-        const root=document.querySelector('#root')
-        root.innerHTML = "<div style='color:red;'><h4>Runtime Error</h4>" + err+ "</div>";
-        throw err;
-    }
-},false);
-</script>
-</html>`
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+      </body>
+      <script>
+      window.addEventListener(
+        'message',
+        (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+            const root = document.querySelector('#root');
+            root.innerHTML =
+              "<div style='color:red;'><h4>Runtime Error</h4>" + err + '</div>';
+            throw err;
+          }
+        },
+        false
+      );</script>
+    </html>`
 
 const Preview: React.FC<PreviewProps> = ({ code }) => {
     const iframe = useRef<any>();
--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,43 @@
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
-import { fetchPlugin } from './plugins/fetch-plugin';
+// import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+// import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './component/code-editor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import Preview from './component/previou';
-
+import bundle from './bunlder/index';
 const App = () => {
     const [value, setValue] = useState('');
     const [code, setCode] = useState('');
-    const ref = useRef<any>();
-    const startService = async () => {
-        const service = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        });
-        ref.current = service;
-    };
-    useEffect(() => {
-        startService();
-    }, []);
+    // const ref = useRef<any>();
+    // const startService = async () => {
+    //     const service = await esbuild.startService({
+    //         worker: true,
+    //         wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+    //     });
+    //     ref.current = service;
+    // };
+    // useEffect(() => {
+    //     startService();
+    // }, []);
     const onClick = async () => {
-        if (!ref.current) {
-            return;
-        };
+        // if (!ref.current) {
+        //     return;
+        // };
         // iframe.current.srcdoc = html;
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window'
-            },
-            plugins: [unpkgPathPlugin(), fetchPlugin(value)]
-        });
+        // const result = await ref.current.build({
+        //     entryPoints: ['index.js'],
+        //     bundle: true,
+        //     write: false,
+        //     define: {
+        //         'process.env.NODE_ENV': '"production"',
+        //         global: 'window'
+        //     },
+        //     plugins: [unpkgPathPlugin(), fetchPlugin(value)]
+        // });
         // iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-        setCode(result.outputFiles[0].text)
+        const output = await bundle(value)
+        setCode(output.outputFiles[0].text)
     };
 
 
@@ -62,21 +62,7 @@ const App = () => {
              </button>
             </div>
             <Preview code={code}></Preview>
-            {/* <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='preview'> */}
-            {/* </iframe> */}
         </div >
-        // <div>
-        //     <textarea value={value} onChange={(e) => setValue(e.target.value)}>
-        //     </textarea>
-        //     <div>
-        //         <button onClick={onClick}>
-        //             Submit
-        //          </button>
-        //     </div>
-        //     <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} title='preview'>
-        //     </iframe>
-        // </div>
-
     )
 };
 
